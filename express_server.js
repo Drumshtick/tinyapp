a74105c3b3da27a05f85ad0675bcca647725d6d6
Drumshtick <ekodnar@outlook.com>
2021-11-10 00:34:13 +0000

    Added if conditional to check for username cookie (inside _header) if it is display it next to login form, fixed cookie-parser (forgot to include app.use statment) in express_server and passed cookie username with templateVars to all pages that render _header

--- express_server.js
+++ express_server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
 const cookieParser = require('cookie-parser');
-const lengthOfMiniURL = 6;
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
 app.set('view engine', 'ejs');
+const lengthOfMiniURL = 6;
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
@@ -17,7 +18,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    username: req.cookies.username,
+    urls: urlDatabase,
+  };
   res.render('urls_index', templateVars);
 });
 
@@ -27,11 +31,16 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  const templateVars = { username: req.cookies.username, };
+  res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies.username,
+  };
   res.render('urls_show', templateVars);
 });
 
@@ -70,7 +79,7 @@ app.post('/urls/:shortURL/updated', (req, res) => {
 
 app.post('/login', (req, res) => {
   const username = req.body.username;
-  res.cookie('username', username)
+  res.cookie('username', username);
   res.redirect('/urls');
 });
 
