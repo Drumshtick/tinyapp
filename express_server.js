73ac2793288acd23ab00c9cdfbee5ae997efa48f
Drumshtick <ekodnar@outlook.com>
2021-11-11 23:29:15 +0000

    Uninstalled/removed dependanct cookie-parser, added some comments to enhance readability, eslint error fix

--- express_server.js
+++ express_server.js
@@ -25,35 +25,35 @@ and the database for shortened URLs
 */
 const urlDatabase = {
   b6UTxQ: {
-      longURL: "https://www.tsn.ca",
-      userID: "userRandomID"
+    longURL: "https://www.tsn.ca",
+    userID: "userRandomID"
   },
   i3BoGr: {
-      longURL: "https://www.google.ca",
-      userID: "user2RandomID"
+    longURL: "https://www.google.ca",
+    userID: "user2RandomID"
   }
 };
 /*
 Password hashes for test users
 */
-const user1Pass = bcrypt.hashSync('123', 10)
-const user2Pass = bcrypt.hashSync('asd', 10)
+const user1Pass = bcrypt.hashSync('123', 10);
+const user2Pass = bcrypt.hashSync('asd', 10);
 /*
 Database for registered users including test
 users
 */
-const users = { 
+const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: user1Pass,
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: user2Pass,
   }
-}
+};
 /*
 =================================================================================
 **************************************GET****************************************
@@ -76,33 +76,38 @@ by the currently logged in user
 */
 app.get('/urls', (req, res) => {
   const { user_id } = req.session;
-  if (user_id === undefined) {
+  if (!user_id) {
     res.redirect('/no_cred');
   } else if (user_id) {
-    const templateVars = { 
+    const templateVars = {
       userObj: users[user_id],
       urls: filterURLsByID(urlDatabase, user_id),
     };
     res.render('urls_index', templateVars);
   }
 });
-
+/*
+Creation of new short URLs 'Create New URL'
+Form for adding longURL
+*/
 app.get('/urls/new', (req, res) => {
   const { user_id } = req.session;
-  if (user_id === undefined) {
+  if (!user_id) {
     res.redirect('/login');
   } else if (user_id) {
     const templateVars = {userObj: users[user_id],};
     res.render('urls_new', templateVars);
   }
 });
-
+/*
+See short and long URL with option to update the long URL
+*/
 app.get('/urls/:shortURL', (req, res) => {
   const { user_id } = req.session;
   const { shortURL } = req.params;
   const entryURL = urlDatabase[shortURL];
   if (!user_id) {
-    res.redirect('/no_cred')
+    res.redirect('/no_cred');
   } else if (!entryURL) {
     res.redirect('/not_found');
   } else if (user_id !== entryURL.userID) {
@@ -116,7 +121,9 @@ app.get('/urls/:shortURL', (req, res) => {
     res.render('urls_show', templateVars);
   }
 });
-
+/*
+Link to follow short URL to long URL
+*/
 app.get('/u/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   if (!urlDatabase[shortURL]) {
@@ -125,11 +132,14 @@ app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[shortURL].longURL;
   res.redirect(addProtocol(longURL));
 });
-
+/*
+Serves register page
+if logged in redirect to urls
+*/
 app.get('/register', (req, res) => {
   const { user_id } = req.session;
   if (user_id) {
-    res.redirect('/urls')
+    res.redirect('/urls');
   } else if (!user_id) {
     const templateVars = {
       userObj: users[user_id],
@@ -137,11 +147,14 @@ app.get('/register', (req, res) => {
     res.render('register', templateVars);
   }
 });
-
+/*
+serves login page
+if logged in redirect to urls
+*/
 app.get('/login', (req, res) => {
   const { user_id } = req.session;
   if (user_id) {
-    res.redirect('/urls')
+    res.redirect('/urls');
   } else if (!user_id) {
     const templateVars = {
       userObj: users[user_id],
@@ -149,7 +162,10 @@ app.get('/login', (req, res) => {
     res.render('login', templateVars);
   }
 });
-
+/*
+serves no credentials page
+suggests to login or register
+*/
 app.get('/no_cred', (req, res) => {
   const { user_id } = req.session;
   const templateVars = {
@@ -157,17 +173,24 @@ app.get('/no_cred', (req, res) => {
   };
   res.render('no_cred', templateVars);
 });
-
+/*
+serves not authorized page
+If user is trying to access a page
+another user only has access to
+*/
 app.get('/not_auth', (req, res) => {
   const { user_id } = req.session;
   const templateVars = {userObj: users[user_id]};
   res.render('not_auth', templateVars);
 });
-
+/*
+serves not found page
+if a short url has not been created yet
+*/
 app.get('/not_found', (req, res) => {
   const { user_id } = req.session;
   const templateVars = {userObj: users[user_id]};
-  res.render('shortURL_not_found', templateVars)
+  res.render('shortURL_not_found', templateVars);
 });
 
 /*
@@ -175,10 +198,15 @@ app.get('/not_found', (req, res) => {
 **1************************************POST***************************************
 =================================================================================
 */
+/*
+Receives form data from 'Create new URL'
+if POSTer has no cookie 403 and redirect them
+to login
+*/
 app.post('/urls', (req, res) => {
   const { user_id } = req.session;
   const { longURL } = req.body;
-  if (user_id === undefined) {
+  if (!user_id) {
     res.redirect(403, '/login');
   } else if (user_id) {
     const shortURL = generateRandomString();
@@ -189,11 +217,15 @@ app.post('/urls', (req, res) => {
     res.redirect(`/urls/${shortURL}`);
   }
 });
-
+/*
+Receives from delete button on /urls page
+deletes data if user id matches cookie and url
+exists
+*/
 app.post('/urls/:shortURL/delete', (req, res) => {
   const { user_id } = req.session;
   const { shortURL } = req.params;
-  if (urlDatabase[shortURL] === undefined) {
+  if (!urlDatabase[shortURL]) {
     res.redirect(404, '/urls');
   } else if (user_id !== urlDatabase[shortURL].userID) {
     res.sendStatus(403);
@@ -202,11 +234,14 @@ app.post('/urls/:shortURL/delete', (req, res) => {
     res.redirect('/urls');
   }
 });
-
+/*
+Form edit button from /urls
+redirects to urls_show to change url
+*/
 app.post('/urls/:shortURL/edit', (req, res) => {
   const { user_id } = req.session;
   const { shortURL } = req.params;
-  if (urlDatabase[shortURL] === undefined) {
+  if (!urlDatabase[shortURL]) {
     res.redirect(404, '/urls');
   } else if (user_id !== urlDatabase[shortURL].userID) {
     res.redirect(403, '/urls');
@@ -216,11 +251,12 @@ app.post('/urls/:shortURL/edit', (req, res) => {
 });
 /*
 The longURL edit form on urls_show POSTs to this branch
+which will update the database with the new URL
 */
 app.post('/urls/:shortURL', (req, res) => {
   const { user_id } = req.session;
   const { shortURL } = req.params;
-  if (urlDatabase[shortURL] === undefined) {
+  if (!urlDatabase[shortURL]) {
     res.redirect(404, '/urls');
   } else if (user_id !== urlDatabase[shortURL].userID) {
     res.redirect(403, '/urls');
@@ -230,7 +266,13 @@ app.post('/urls/:shortURL', (req, res) => {
     res.redirect(`/urls/${shortURL}`);
   }
 });
-
+/*
+login page form
+checks to ensure email and password arent empty
+checks to see if account exists
+if exists redirect to urls and create session cookie
+if logged in redirect client to urls
+*/
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -248,12 +290,20 @@ app.post('/login', (req, res) => {
     res.redirect(403, '/login');
   }
 });
-
+/*
+logout button deletes session cookie and redirects to urls
+*/
 app.post('/logout', (req, res) => {
   req.session = null;
   res.redirect('/urls');
 });
-
+/*
+Create account page form
+checks to ensure email and password arent empty
+checks to see if account exists if it does redirect
+if already exists redirect login
+if logged in redirect client to urls
+*/
 app.post('/register', (req, res) => {
   const { email, password} = req.body;
   if (!email || !password) {
@@ -271,7 +321,7 @@ app.post('/register', (req, res) => {
     req.session.user_id = userID;
     res.redirect('/urls');
   } else if (!verifyNewEmail(users, email)) {
-    res.redirect(400, '/register');
+    res.redirect(400, '/login');
   }
 });
 
